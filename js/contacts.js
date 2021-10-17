fe0bb7b89ce5cebe924be4ac02205024ac2807f0
CGG <16348315+C-G-G@users.noreply.github.com>
2021-10-16 20:50:27 -0400

    Create a data structure for contacts and display it

---
+++ js/contacts.js
@@ -0,0 +1,26 @@
+var peeps = [
+  {
+    name: 'Mercury Man',
+    email: 'theman@mercury.com',
+    tel: '+05551234',
+    loc: [46001200, 69816900]
+  },
+  {
+    name: 'Venus Venturer',
+    email: 'venus@theadventurer.com',
+    tel: '+15551234',
+    loc: [107477000, 108939000]
+  },
+  {
+    name: 'Jupiter Juggernaut',
+    email: 'thejugger@jupiter.com',
+    tel: '+55551234',
+    loc: [816040000, 74050000]
+  },
+  {
+    name: 'Mars van Red',
+    email: 'thered@mars.com',
+    tel: '+85551234',
+    loc: [777040000, 65650000]
+  }
+];
